4ded58edcd1162cc9df42b580df846c99d94a51f
cjstuff <chiragjain9724@gmail.com>
2022-11-26 19:24:25 +0530

    chnaged path in index and added static

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'))
 });
 
 const server = http.createServer(app)
