affc511e0ca877baa2b1c0363f06b3b27f37010a
cjstuff <chiragjain9724@gmail.com>
2022-11-29 18:40:31 +0530

    changes for netlify

--- server/index.ts
+++ server/index.ts
@@ -21,10 +21,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-  });
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  // });
 }
 
 const server = http.createServer(app)
