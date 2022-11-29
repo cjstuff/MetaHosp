0b66a6d4813891403bbf97d81c5c1f041306c970
cjstuff <chiragjain9724@gmail.com>
2022-11-29 16:42:37 +0530

    modified index.ts

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('client/dist'))
+//app.use(express.static('client/dist'))
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
