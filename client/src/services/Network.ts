a2a718ac424a07f3298e27dc59f49041d0037b16
cjstuff <chiragjain9724@gmail.com>
2022-11-29 16:07:04 +0530

    chnages for onrender

--- client/src/services/Network.ts
+++ client/src/services/Network.ts
@@ -33,7 +33,7 @@ export default class Network {
     const protocol = window.location.protocol.replace('http', 'ws')
     const endpoint =
       process.env.NODE_ENV === 'production'
-        ? `wss://metahosp-app.herokuapp.com/`
+        ? `wss://metahosp.onrender.com/`
         : `${protocol}//${window.location.hostname}:2567`
     this.client = new Client(endpoint)
     this.joinLobbyRoom().then(() => {
