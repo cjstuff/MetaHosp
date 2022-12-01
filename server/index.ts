5ef3fefcc1b39be4bb188b89a617b7dc67743d1a
cjstuff <chiragjain9724@gmail.com>
2022-12-02 00:34:38 +0530

    cors changes

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ const app = express()
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://metahosp.herokuapp.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
